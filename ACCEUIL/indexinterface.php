27bd775b1f282538794a17cc07733b3a35d1da85
SAE303 <sae303tp4@gmail.com>
2024-11-21 16:32:09 +0100

    Add files via upload

---
+++ ACCEUIL/indexinterface.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projet Sportif Universitaire</title>
+    <link rel="stylesheet" href="interface.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>SAE 303</h1>
+        </header>
+        <div class="content-wrapper">
+            <section class="logos-side left">
+                <img src="img/football.png" alt="Football" class="logo">
+                <img src="img/basket.png" alt="Basketball" class="logo">
+                <img src="img/tennis.png" alt="Tennis" class="logo">
+                <img src="img/equitation.png" alt="Équitation" class="logo">
+                <img src="img/golf.png" alt="Golf" class="logo">
+            </section>
+            <section class="context">
+                <p>
+                    Ce projet vise à explorer l'impact et les tendances des fédérations sportives à travers une analyse
+                    des données. L'objectif est de mieux comprendre la popularité de différents sports et de se rendre compte de leurs répartition sur le territoire français. 
+                </p>
+                <a href="https://sae303.github.io/SAE-303/" class="link-button">Accéder à la Page</a>
+            </section>
+            <section class="logos-side right">
+                <img src="img/football.png" alt="Football" class="logo">
+                <img src="img/basket.png" alt="Basketball" class="logo">
+                <img src="img/tennis.png" alt="Tennis" class="logo">
+                <img src="img/equitation.png" alt="Équitation" class="logo">
+                <img src="img/golf.png" alt="Golf" class="logo">
+            </section>
+        </div>
+    </div>
+</body>
+</html>
